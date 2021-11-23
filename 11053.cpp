99ba49f803a49b4af96ea19890d8f9506d0f4c41
BulTTaeng <60182208+Choi-JaeHyeok-21500749@users.noreply.github.com>
2021-11-23 22:19:27 +0900

    Update 11053.cpp

--- 11053.cpp
+++ 11053.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 vector<int> A;
 
-int dp[1001][2];
+int dp[1001];
 
 int main() {
   int N , input;
@@ -16,8 +16,7 @@ int main() {
     cin >> input;
     A.push_back(input);
   }
-  dp[0][0] = 1;
-  dp[0][1] = A[0];
+  dp[0] = 1;
   
   int ans = 0;
   int tmp_num, tmp_c;
@@ -28,21 +27,21 @@ int main() {
 
     for(int j = 0; j<i; j++){
       if(A[j] < tmp_num){
-        if(tmp_c <= dp[j][0]){
-          tmp_c = dp[j][0] +1;
+        if(tmp_c <= dp[j]){
+          tmp_c = dp[j] +1;
         }
       }
     }
     if(tmp_c == 0){
       tmp_c = 1;
     }
-    dp[i][0] = tmp_c;
+    dp[i] = tmp_c;
   }
 
   for(int i =0; i<N; i++){
     //cout << dp[i][0] << " ";
-    if(ans < dp[i][0]){
-      ans = dp[i][0];
+    if(ans < dp[i]){
+      ans = dp[i];
     }
   }
 
